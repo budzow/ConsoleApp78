302c5cb2eb8fd81f414d7635b2e36d45369055c5
Michal Budzowski <michal.budzowski@sonarsource.com>
2024-10-08 09:18:47 +0200

    Add project files.

---
+++ ConsoleApp78/ExampleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("Example")]
+public class ExampleController : ControllerBase
+{
+    private const string TargetDirectory = "/path/to/target/directory";
+
+    public IActionResult FileExists()
+    {
+        string file = Request.Query["file"];
+        string path = TargetDirectory + file;
+        if (!System.IO.File.Exists(path))
+        { // Noncompliant
+            return NotFound("File not found");
+        }
+        return Ok("File found");
+    }
+}
---
+++ build9.bat
@@ -0,0 +1,4 @@
+"SonarScanner.MSBuild.exe" begin /k:"ConsoleApp78" /d:sonar.verbose=true /v:2 /d:sonar.host.url=http://localhost:9000
+rem /d:sonar.projectDate=2023-03-30
+msbuild /t:rebuild 
+"SonarScanner.MSBuild.exe" end
